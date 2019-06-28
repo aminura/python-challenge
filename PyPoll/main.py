70fd5a3eff964a3a60afb2581a9cb0db869a4b8d
Arunima Menon <arunima_menon@zusc-s-dc02.airbnb.biz>
2019-06-28 14:36:19 -0700

    Rewritten code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,96 +1,66 @@
-# Import dependencies
-import os, csv
-from pathlib import Path 
+import os
+import csv
 
-# Assign file location with the pathlib library
-csv_file_path = Path("python-challenge", "PyPoll", "election_data.csv")
+csvpath = os.path.join("election_data.csv")
 
-# Declare Variables 
-total_votes = 0 
-khan_votes = 0
-correy_votes = 0
-li_votes = 0
-otooley_votes = 0
+#Initializing Values of the variables and lists
+totalVotes = 0
+candidates = []
+voteCount = []
+winnerVoteCount = 0
 
-# Open csv in default read mode with context manager
-with open(csv_file_path,newline="", encoding="utf-8") as elections:
+#Reading data from the file election_data.csv
+with open(csvpath, 'r', newline='') as csvfile:        
+    
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #Reading header data in csv_header
+    csv_header = next(csvreader)
 
-    # Store data under the csvreader variable
-    csvreader = csv.reader(elections,delimiter=",") 
+    #Looping through each data record in the file
+    for row in csvreader:
+        #Incrementing vote counter for each record
+        totalVotes += 1
 
-    # Skip the header so we iterate through the actual values
-    header = next(csvreader)     
+        #Creating a list of distinct candidates 
+        if(row[2] not in candidates):
+            candidates.append(row[2])
+            voteCount.append(0)
+        
+        #Getting the Index in candidate list 
+        candidateIndex = candidates.index(row[2])
+        #Using the same Index to increment count of votes for the candidate
+        voteCount[candidateIndex] += 1
 
-    # Iterate through each row in the csv
-    for row in csvreader: 
+    #Printing output on console
+    print(f"\nElection Results\n-------------------------------------------")
+    print(f"Total votes: {totalVotes}")
+    print("-------------------------------------------")
+    
+    for x in range(len(candidates)):
+        votePercent = round((voteCount[x]/totalVotes)*100,3)
+        print(f"{candidates[x]}: {votePercent}% ({voteCount[x]})")
+        if (winnerVoteCount<voteCount[x]):
+            winnerVoteCount = voteCount[x]
+            winner = candidates[x]
+    
+    print("-------------------------------------------")
+    print(f"Winner: {winner}")
+    print("-------------------------------------------")
 
-        # Count the unique Voter ID's and store in variable  called total_votes
-        total_votes +=1
+file = open('output.txt','w')
 
-        # We have four candidates if the name is found, count the times it appears and store in a list
-        # We can use this values in our percent vote calculation in the print statements
-        if row[2] == "Khan": 
-            khan_votes +=1
-        elif row[2] == "Correy":
-            correy_votes +=1
-        elif row[2] == "Li": 
-            li_votes +=1
-        elif row[2] == "O'Tooley":
-            otooley_votes +=1
+#Writing output to the file    
+file.write("Election Results")
+file.write("\n-------------------------------------------")
+file.write("\nTotal votes:" + str(totalVotes))
+file.write("\n-------------------------------------------")
+    
+for x in range(len(candidates)):
+    votePercent = round((voteCount[x]/totalVotes)*100,3)
+    file.write("\n" + str(candidates[x]) +" : " + str(votePercent) 
+                + "% ("+ str(voteCount[x]) + ")")
+file.write("\n-------------------------------------------")
+file.write("\nWinner: " + str(winner))
+file.write("\n-------------------------------------------")
 
- # To find the winner we want to make a dictionary out of the two lists we previously created 
-candidates = ["Khan", "Correy", "Li","O'Tooley"]
-votes = [khan_votes, correy_votes,li_votes,otooley_votes]
-
-# We zip them together the list of candidate(key) and the total votes(value)
-# Return the winner using a max function of the dictionary 
-dict_candidates_and_votes = dict(zip(candidates,votes))
-key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
-
-# Print a the summary of the analysis
-khan_percent = (khan_votes/total_votes) *100
-correy_percent = (correy_votes/total_votes) * 100
-li_percent = (li_votes/total_votes)* 100
-otooley_percent = (otooley_votes/total_votes) * 100
-
-# Print the summary table
-print(f"Election Results")
-print(f"----------------------------")
-print(f"Total Votes: {total_votes}")
-print(f"----------------------------")
-print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
-print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
-print(f"Li: {li_percent:.3f}% ({li_votes})")
-print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
-print(f"----------------------------")
-print(f"Winner: {key}")
-print(f"----------------------------")
-
-# Output files
-# Assign output file location and with the pathlib library
-output_file = Path("python-challenge", "PyPoll", "Election_Results_Summary.txt")
-
-with open(output_file,"w") as file:
-
-# Write methods to print to Elections_Results_Summary 
-    file.write(f"Election Results")
-    file.write("\n")
-    file.write(f"----------------------------")
-    file.write("\n")
-    file.write(f"Total Votes: {total_votes}")
-    file.write("\n")
-    file.write(f"----------------------------")
-    file.write("\n")
-    file.write(f"Khan: {khan_percent:.3f}% ({khan_votes})")
-    file.write("\n")
-    file.write(f"Correy: {correy_percent:.3f}% ({correy_votes})")
-    file.write("\n")
-    file.write(f"Li: {li_percent:.3f}% ({li_votes})")
-    file.write("\n")
-    file.write(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
-    file.write("\n")
-    file.write(f"----------------------------")
-    file.write("\n")
-    file.write(f"Winner: {key}")
-    file.write("\n")
-    file.write(f"----------------------------")
+file.close()
